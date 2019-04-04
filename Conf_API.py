50a6b9a2a9dc6342704f3d379630513b76542c0d
roovyshapiro <roovyshapiro@gmail.com>
2019-04-04 13:27:55 -0400

    add timedelta date calculation
    added some test functions to better understand how times are
    calculated and subtracted from each other. This will be important
    as ultimately the program must have a way to specify the date
    from where to gather data.

--- Conf_API.py
+++ Conf_API.py
@@ -1,29 +1,33 @@
-def conf_test():
-    import requests, json, datetime
-    from requests.auth import HTTPBasicAuth
+import requests, json, datetime
+from datetime import date, timedelta
+from requests.auth import HTTPBasicAuth
 
-    host = ""
-    username = ""
-    api_key = ""
+def conf_test(year, month, day):
+    host = "companyurl.atlassian.net"
+    username = "user@companyurl.com"
+    api_key = "padVOcwy3jty3O2BsyNHxSI5"
 
     ##Confluence uses unix epoch time stamps which is milliseconds seconds Jan, 1st 1970.
+    ##https://currentmillis.com/ -> Useful site for conversions
     ##1 second = 1000 milliseconds
     ##1 day = 86400 seconds
-    ##7 days = 602,000 seconds
-    ##7 days = 602,000,000 milliseconds
+    ##7 days = 604,800 seconds
+    ##7 days = 604,800,000 milliseconds
     ##14 days = 1,209,600
+
     ##14 days = 1,209,600,000 milliseconds
-
+    #Supply the date and retrieves the timestamp in milliseconds
+    timestamp = int((datetime.datetime(year, month, day, 0, 0).timestamp()) * 1000)
+    
+    #get current time in unix epoch milliseconds since confluence uses miliseconds.
     current_time = int((datetime.datetime.timestamp(datetime.datetime.now())) * 1000)
-    two_weeks = 1209600000
-    start_time = current_time - two_weeks
-    end_time = current_time
 
-    url = f"https://{host}/wiki/rest/api/audit?startDate={str(start_time)}&endDate={str(end_time)}"
+    one_week = 604800000
+    start_time = timestamp - one_week
+
+    url = f"https://{host}/wiki/rest/api/audit?startDate={str(start_time)}&endDate={str(current_time)}"
 
     auth = HTTPBasicAuth(username, api_key)
-
-    #get current time in unix epoch milliseconds since confluence uses miliseconds.
 
     headers = {
        "Accept": "application/json"
@@ -38,3 +42,29 @@ def conf_test():
 
     data = response.json()
     return data
+
+def time_diff(year, month, day):
+    '''
+    subtract the current date by 7 days.
+    dt = date.today() - timedelta(7)
+    >>>dt
+    datetime.date(2019, 4, 3)
+    >>>str(dt).split('-')
+    ['2019', '04', '03']
+
+    >>>str(date.today()).split('-')
+    ['2019', '04', '04']
+    >>>str(date.today() - timedelta(7)).split('-')
+    ['2019', '03', '28']
+    '''
+    timestamp = int((datetime.datetime(year, month, day, 0, 0).timestamp() * 1000))
+    date = datetime.date(year, month, day)
+    last_week = date - timedelta(7)
+    dt = str(last_week).split('-')
+    week_prior_timestamp = int((datetime.datetime(int(dt[0]), int(dt[1]), int(dt[2]), 0, 0).timestamp() * 1000))
+    
+    print(f"7 days prior is {dt[1]} {dt[2]} {dt[0]}")
+    print(f"The first timestamp is {timestamp}, and one week prior is {week_prior_timestamp}")
+    print(f"The difference between the two timestamps is {timestamp - week_prior_timestamp}")
+    return ''
+
